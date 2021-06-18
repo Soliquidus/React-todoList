a82c33fee1f32a955892511293fc00dddc2b4f1e
Soliquidus <tibo.pfeifer@gmail.com>
2021-06-18 18:29:25 +0200

    TodoList.js and TodoItem.js with fetch and delete methods added

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'materialize-css/dist/css/materialize.css'
+
+function TodoList() {
+    return (
+        <div>
+            <h1>TodoList</h1>
+        </div>
+    );
+}
+
+export default TodoList;
